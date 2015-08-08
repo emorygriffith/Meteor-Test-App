4d3be128d3240324963af34f0bf3f6fe85e0bcd8
Emory Griffith <emorygriffith91@gmail.com>
2015-08-07 23:43:41 -0400

    set up styles and mongoDB

--- MeteorTestApp.js
+++ MeteorTestApp.js
@@ -9,6 +9,22 @@ if (Meteor.isClient) {
 
   });
 
+    Template.body.events({
+  "submit .new-task": function (event) {
+    // Prevent default browser form submit
+    event.preventDefault();
 
+    // Get value from form element
+    var text = event.target.text.value;
 
+    // Insert a task into the collection
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    event.target.text.value = "";
+  }
+  });
 }
